e00b2e04c2095bf387116499d039bcbd58dc755f
Tudor Bujdei-Leonte <tudor.bujdei-leonte@student.manchester.ac.uk>
2021-10-20 13:16:20 +0100

    Added tests to check correct patterns in Gamblos Reverse Arrow Quest

--- tests/games/stendhal/server/maps/quests/ReverseArrowTest.java
+++ tests/games/stendhal/server/maps/quests/ReverseArrowTest.java
@@ -13,17 +13,22 @@
 package games.stendhal.server.maps.quests;
 
 import static org.junit.Assert.assertEquals;
+// import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+// import static org.junit.Assert.assertFalse;
+// import static org.junit.Assert.assertTrue;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+// import games.stendhal.common.Direction;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.entity.mapstuff.portal.Door;
 import games.stendhal.server.entity.mapstuff.sign.Sign;
 import games.stendhal.server.entity.npc.NPCList;
+// import games.stendhal.server.entity.player.Player;
 import games.stendhal.server.maps.MockStendlRPWorld;
 import marauroa.common.game.RPClass;
 import utilities.PlayerTestHelper;
@@ -88,4 +93,80 @@ public class ReverseArrowTest {
 		assertNull(arrowquest.player);
 	}
 
+	/**
+	 * Tests for validity of arrow shape
+	 */
+	// @Test
+	// public void testCorrectPattern1() {
+	// 	ReverseArrow arrowquest = new ReverseArrow();
+	// 	arrowquest.addToWorld();
+	// 	Player bob = PlayerTestHelper.createPlayer("bob");
+	// 	PlayerTestHelper.registerPlayer(bob, SingletonRepository.getRPWorld().getZone("int_ados_reverse_arrow"));
+
+	// 	arrowquest.start(bob);
+
+	// 	assertTrue(arrowquest.player.isQuestCompleted("reverse_arrow"));
+	// }
+
+	@Test
+	public void testWrongPattern(){
+		// 0 1 2 3 4
+		// * 5 6 7 *
+		// * * 8 * *   
+		ReverseArrow arrowquest = new ReverseArrow();
+		arrowquest.addToWorld();
+
+		arrowquest.player = PlayerTestHelper.createPlayer("bob");
+		arrowquest.zone.add(arrowquest.player);
+		assertEquals(arrowquest.zone, arrowquest.player.getZone());
+
+		arrowquest.start(arrowquest.player);
+		assertEquals(null, arrowquest.player.getQuest(arrowquest.getName()));
+		
+		arrowquest.new ReverseArrowCheck().onTurnReached(0);
+		assertEquals("failed", arrowquest.player.getQuest(arrowquest.getSlotName()));
+	}
+	
+	@Test
+	public void testCorrectPattern1() {
+		System.out.println("Started test correct pattern");
+		// * * 8 * *
+		// * 1 2 3 *
+		// 0 5 6 7 4     
+		ReverseArrow arrowquest = new ReverseArrow();
+		arrowquest.addToWorld();
+
+		arrowquest.player = PlayerTestHelper.createPlayer("bob");
+		arrowquest.zone.add(arrowquest.player);
+		arrowquest.start(arrowquest.player);
+
+		arrowquest.tokens.get(8).setPosition(arrowquest.tokens.get(8).getX(), arrowquest.tokens.get(8).getY()-3);
+		arrowquest.tokens.get(0).setPosition(arrowquest.tokens.get(0).getX(), arrowquest.tokens.get(0).getY()+1);
+		arrowquest.tokens.get(4).setPosition(arrowquest.tokens.get(4).getX(), arrowquest.tokens.get(4).getY()+1);
+		
+		arrowquest.new ReverseArrowCheck().onTurnReached(0);
+		assertEquals("done", arrowquest.player.getQuest(arrowquest.getSlotName()));
+	}
+	
+	@Test
+	public void testCorrectPattern2() {
+		System.out.println("Started test correct pattern");
+		// * * 2 * *
+		// * 1 * 3 *
+		// 0 5 6 7 4
+		// * * 8 * *
+		ReverseArrow arrowquest = new ReverseArrow();
+		arrowquest.addToWorld();
+
+		arrowquest.player = PlayerTestHelper.createPlayer("bob");
+		arrowquest.zone.add(arrowquest.player);
+		arrowquest.start(arrowquest.player);
+
+		arrowquest.tokens.get(2).setPosition(arrowquest.tokens.get(2).getX(), arrowquest.tokens.get(2).getY()-1);
+		arrowquest.tokens.get(0).setPosition(arrowquest.tokens.get(0).getX(), arrowquest.tokens.get(0).getY()+1);
+		arrowquest.tokens.get(4).setPosition(arrowquest.tokens.get(4).getX(), arrowquest.tokens.get(4).getY()+1);
+		
+		arrowquest.new ReverseArrowCheck().onTurnReached(0);
+		assertEquals("done", arrowquest.player.getQuest(arrowquest.getSlotName()));
+	}
 }
